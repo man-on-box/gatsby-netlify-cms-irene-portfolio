320f46642d87ad75411fc4ddf0ed1e2d66ba1d24
Oliver Hughes <oliverhughes@admins-MacBook-Pro-2.local>
2020-07-17 22:05:57 +0200

    add preview page

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import cx from "classnames";
 import PreviewCompatibleImage from "@components/PreviewCompatibleImage";
-import { HTMLContent } from "@components/Content";
+import { Content, HTMLContent } from "@components/Content";
 import { SectionHeader } from "@components/SectionHeader";
 import { ImageInfo } from "types/ImageInfo";
 import styles from "./index.module.scss";
@@ -10,25 +10,35 @@ export interface AboutProps {
   title: string;
   imageInfo: ImageInfo;
   content: string;
+  contentComponent?: React.FC<any>;
 }
 
-const About: FC<AboutProps> = ({ title, imageInfo, content }) => (
-  <section className={cx(styles.section, "section")}>
-    <div className="container">
-      <div className="columns">
-        <div className="column">
-          <div className="section">
-            <SectionHeader>{title}</SectionHeader>
+const About: FC<AboutProps> = ({
+  title,
+  imageInfo,
+  content,
+  contentComponent,
+}) => {
+  const PageContent = contentComponent || Content;
 
-            <HTMLContent className="content" content={content} />
+  return (
+    <section className={cx(styles.section, "section")}>
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <div className="section">
+              <SectionHeader>{title}</SectionHeader>
+
+              <PageContent className="content" content={content} />
+            </div>
+          </div>
+          <div className="column">
+            <PreviewCompatibleImage imageInfo={imageInfo} />
           </div>
         </div>
-        <div className="column">
-          <PreviewCompatibleImage imageInfo={imageInfo} />
-        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export { About };
