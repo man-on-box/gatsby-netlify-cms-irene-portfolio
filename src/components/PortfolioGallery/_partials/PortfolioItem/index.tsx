74aa181572077347396b5f4297012523e5becf4c
man-on-box <46682658+man-on-box@users.noreply.github.com>
2020-07-25 17:29:54 +0200

    Moving images to lazy load (#11)
    Co-authored-by: Oliver Hughes <oliverhughes@admins-MacBook-Pro-2.local>

--- src/components/PortfolioGallery/_partials/PortfolioItem/index.tsx
+++ src/components/PortfolioGallery/_partials/PortfolioItem/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import { navigate } from "gatsby";
 import cx from "classnames";
+import PreviewCompatibleImage from "@components/PreviewCompatibleImage";
 import { GatsbyImageProps } from "gatsby-image";
 
 import styles from "./index.module.scss";
@@ -24,17 +25,16 @@ const PortfolioItem: FC<PortfolioItemProps> = ({
     className={cx("column", className, styles.portfolioWrapper)}
     onClick={() => navigate(slug)}
   >
-    <div
-      className={styles.portfolioContent}
-      style={{
-        backgroundImage: `url(${
-          image.childImageSharp && !Array.isArray(image.childImageSharp.fluid)
-            ? image.childImageSharp?.fluid?.src
-            : image
-        })`,
-      }}
-    >
-      <div className={cx("px-6 py-6", styles.withOverlay)}>
+    <div className={styles.portfolioContent}>
+      <div className={styles.positionAbsolute}>
+        <PreviewCompatibleImage
+          imageInfo={image}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </div>
+      <div
+        className={cx("px-6 py-6", styles.positionAbsolute, styles.withOverlay)}
+      >
         <p
           className={cx(
             "title is-4 has-text-weight-bold is-bold-light",
