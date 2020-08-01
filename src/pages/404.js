46b398fda841ca1be2e5c2bc77248c06ed151a0f
man-on-box <46682658+man-on-box@users.noreply.github.com>
2020-08-01 17:18:26 +0200

    Add custom not found page (#14)
    Co-authored-by: Oliver Hughes <oliverhughes@admins-MacBook-Pro-2.local>

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
+import { NotFound } from "../screens/NotFound/index.tsx";
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <NotFound />
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
